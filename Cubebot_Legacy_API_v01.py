dbe50110f4762b1ada25443bf07ddbf020e9d128
vitorshaft <vtrshaft@gmail.com>
2022-06-28 20:36:17 -0300

    Inserido rastreamento do cubesat por range de cor grayscale

--- Cubebot_Legacy_API_v01.py
+++ Cubebot_Legacy_API_v01.py
@@ -67,13 +67,45 @@ if clientID!=-1:
     # Executa a sequência de movimento
     sim.simxCallScriptFunction(clientID,Manipulador,sim.sim_scripttype_childscript,'legacy_API_Movimento',[],[],[],'SeqMov',sim.simx_opmode_oneshot)
     
+    #resolution, image = sim.simxGetVisionSensorImage(clientID, v1, 0, sim.simx_opmode_buffer)
+    #linhas, cols, _ = image.shape   #obtem numero de linhas e colunas da imagem
+    linhas = 320
+    cols = 240
+    x_medio = int(cols / 2)         #calcula x do meio da tela
+    y_medio = int(linhas/2)         #calcula y do meio da tela
+    centro = int(cols / 2)          #mesma coisa, mas essa variavel nao vai mudar
+    centroY = int(linhas/2)         
+    posicao = 90 # degrees          #valor inicial do motor do robo
+
     while (sim.simxGetConnectionId(clientID) != -1):
         err, resolution, image = sim.simxGetVisionSensorImage(clientID, v1, 0, sim.simx_opmode_buffer)
+        
         if err == sim.simx_return_ok:
             print("image OK!!!")
             img = np.array(image,dtype=np.uint8)
             img.resize([resolution[1],resolution[0],3])
-            cv2.imshow('image',img)
+            menor = 10
+            maior = 35
+            low_cinza = np.array([menor,menor,menor])    #menor RGB possivel
+            high_cinza = np.array([maior,maior,maior])   #maior RGB possivel
+            cinza_mask = cv2.inRange(img,low_cinza,high_cinza)    #cria intervalo de cores (mascara)
+            contornos, _ = cv2.findContours(cinza_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) #procura contornos da cor detectada
+            
+            contornos = sorted(contornos, key=lambda x:cv2.contourArea(x), reverse=True)    #inverte a ordem dos contornos            
+            for cnt in contornos:
+                (x, y, w, h) = cv2.boundingRect(cnt)
+                if (((x_medio**2)-(int((x + x + w) / 2))**2)>16):
+                    x_medio = int((x + x + w) / 2)
+                    y_medio = int((y + y + h) / 2)
+                break
+            
+            cv2.line(img, (x_medio, 0), (x_medio, 320), (0, 255, 0), 1)
+            cv2.line(img, (0, y_medio), (240, y_medio), (0,255,0), 1)
+            cv2.line(cinza_mask, (x_medio, 0), (x_medio, 320), (255, 255, 255), 1)
+            cv2.line(cinza_mask, (0, y_medio), (240, y_medio), (255,255,255), 1)
+            #cv2.imshow('image',image)
+            cv2.imshow('POV',img)
+            cv2.imshow('P/B',cinza_mask)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         elif err == sim.simx_return_novalue_flag:
