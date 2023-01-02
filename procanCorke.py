5a8817ad66aead2495ecdfa8169b23e60f23d368
vitorshaft <vtrshaft@gmail.com>
2023-01-02 08:56:26 -0300

    testes com configuracoes do PROCaN

--- procanCorke.py
+++ procanCorke.py
@@ -6,17 +6,28 @@ from spatialmath.base import *
 from spatialmath import SE3
 import spatialmath.base.symbolic as sym
 
-pi = math.pi
+'''
+DH = [[0, 0, -0.1160, 0],    #i1 - base
+    [0, 90*math.pi/180, 0, 0.0294],   #i2 - i1
+    [180*math.pi/180, 180*math.pi/180, 0.23, 0], #i3 - i2
+    [-90*math.pi/180, 90*math.pi/180, 0.08173, -0.00286], #i4 - i3 (multiplicar Z, depois X)
+    [180*math.pi/180, 90*math.pi/180, 0.0019, -0.0904],  #i5 - i4
+    [-90*math.pi/180, -90*math.pi/180, 0.0438, 0],     #i6 - i5
+    #[-180*math.pi/180, -90*math.pi/180, 0.0438, 0],
+    [0., 0., 0., 0.]]
+'''
 
+pi = math.pi
+#d*Rz*a*Rx
 robot = rtb.robot.DHRobot(
     [
-        rtb.robot.RevoluteDH(a=-0.116),
+        #rtb.robot.RevoluteDH(a=-0.116),
         rtb.robot.RevoluteDH(d=0.0294, alpha=pi/2),
-        rtb.robot.RevoluteDH(a=0.23, alpha=pi),
-        rtb.robot.RevoluteDH(d=-0.00286, a=0.08173, alpha=pi/2),
+        rtb.robot.RevoluteDH(a=0.23),# alpha=pi),
+        rtb.robot.RevoluteDH(d=-0.00286, a=0.08173, alpha=-pi/2),
         rtb.robot.RevoluteDH(d=-0.0904, a=0.0019, alpha=pi/2),
-        rtb.robot.RevoluteDH(a=0.0438, alpha=-pi/2)#,
-        #rtb.robot.RevoluteDH()
+        rtb.robot.RevoluteDH(a=0.0438, alpha=-pi/2),#,
+        rtb.robot.RevoluteDH()
     ], name="PROCaN")
 
 print(robot)
